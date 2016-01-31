39d0e4c577af9852d7c83f9572539d445a3d72f3
YukiKitagawa <Yuki-Kitagawa@kitagawayuuki-no-MacBook-Pro.local>
2016-01-31 16:41:06 +0900

    up

--- js/ajax.js
+++ js/ajax.js
@@ -4,13 +4,13 @@ $(function(){
   $('#btn').on('click',function(){
     //ajaxでロード
     $.ajax({
-      url:'omikuji.html',
+      url:'mydata.json',
       type:'GET',
-      dataType:'html'
+      dataType:'json'
     }).done(function(data){
       //成功
       console.log(data);
-      showCondent(data);
+      // showCondent(data);
     }).fail(function(){
       //失敗
       console.log('ajax ERROR');
@@ -24,5 +24,5 @@ $(function(){
 //表示させる関数
   function showCondent(data){
     $('#container').append(data);
-  };
+  }
 });
