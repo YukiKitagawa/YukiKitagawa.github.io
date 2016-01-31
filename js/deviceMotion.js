7fac9e1876a0575769582fc7d07616ad67371848
YukiKitagawa <Yuki-Kitagawa@kitagawayuuki-no-MacBook-Pro.local>
2016-01-31 15:05:32 +0900

    orientation

--- js/deviceMotion.js
+++ js/deviceMotion.js
@@ -1,21 +1,34 @@
 $(function(){
 
   //加速度のイベント
-  window.addEventListener('devicemotion', shakeDevice);
+  // window.addEventListener('devicemotion', shakeDevice);
+  //
+  // function shakeDevice(evt){
+  //   var x = evt.acceleration.x;
+  //   var y = evt.acceleration.y;
+  //   var z = evt.acceleration.z;
+  //
+  //   $('#device li').eq(0).text('加速度X: ' + x);
+  //   $('#device li').eq(1).text('加速度Y: ' + y);
+  //   $('#device li').eq(2).text('加速度Z: ' + z);
+  //
+  //
+  //   if(Math.abs(x) > 5 || Math.abs(y) > 5 || Math.abs(z) > 5){
+  //     $('#result').text('振ったよ');
+  //   }
+  // }
 
-  function shakeDevice(evt){
-    var x = evt.acceleration.x;
-    var y = evt.acceleration.y;
-    var z = evt.acceleration.z;
+  //傾き
+  window.addEventListener('deviceorientation', roteDevice);
 
-    $('#device li').eq(0).text('加速度X: ' + x);
-    $('#device li').eq(1).text('加速度Y: ' + y);
-    $('#device li').eq(2).text('加速度Z: ' + z);
+  function roteDevice(evt){
+      var x = evt.beta;
+      var y = evt.gamma;
+      var z = evt.alpha;
 
-
-    if(Math.abs(x) > 5 || Math.abs(y) > 5 || Math.abs(z) > 5){
-      $('#result').text('振ったよ');
-    }
+        $('#device li').eq(0).text('傾きX: ' + x);
+        $('#device li').eq(1).text('傾きY: ' + y);
+        $('#device li').eq(2).text('傾きZ: ' + z);
   }
 
 });
