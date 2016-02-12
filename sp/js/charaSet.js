649f7d396b72b8c5b9ae8963e4b1f356be564cd3
YukiKitagawa <Yuki-Kitagawa@kitagawayuuki-no-MacBook-Pro.local>
2016-02-12 15:50:35 +0900

    aa

--- sp/js/charaSet.js
+++ sp/js/charaSet.js
@@ -114,11 +114,34 @@ $(function(){
   //----------------------------------------------------------------
   //サイコロ画面
 
+
+  //加速度のイベント
+  window.addEventListener('devicemotion', shakeDevice);
+
+  function shakeDevice(evt){
+    var x = evt.acceleration.x;
+    var y = evt.acceleration.y;
+    var z = evt.acceleration.z;
+
+    $('#device li').eq(0).text('加速度X: ' + x);
+    $('#device li').eq(1).text('加速度Y: ' + y);
+    $('#device li').eq(2).text('加速度Z: ' + z);
+
+
+    if(Math.abs(x) > 5 || Math.abs(y) > 5 || Math.abs(z) > 5){
+      $('#dice li').eq(rand).show();
+    }
+
+
+
+
+
+
   $('#rool').on('click',function(){
     $('#rool').hide();
     rand = Math.floor( Math.random() * 6 ) ;
     console.log(rand);
-    $('#dice li').eq(rand).show();
+
     rand = 0;
 
 
