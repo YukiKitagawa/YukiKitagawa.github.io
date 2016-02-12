c2ae16043ca2dff510c910330990ab9f1233caed
YukiKitagawa <Yuki-Kitagawa@kitagawayuuki-no-MacBook-Pro.local>
2016-02-12 16:17:42 +0900

    aa

--- sp/js/charaSet.js
+++ sp/js/charaSet.js
@@ -6,9 +6,9 @@ $(function(){
   var life = 6;
   var rand = 0;
 
-  // var x = 0;
-  // var y = 0;
-  // var z = 0;
+  var x = 0;
+  var y = 0;
+  var z = 0;
 
 
 
@@ -25,134 +25,134 @@ $(function(){
 
   window.addEventListener('devicemotion', shakeDevice);
 
-    function shakeDevice(evt){
-      var x = evt.acceleration.x;
-      var y = evt.acceleration.y;
-      var z = evt.acceleration.z;
+  function shakeDevice(evt){
+    x = evt.acceleration.x;
+    y = evt.acceleration.y;
+    z = evt.acceleration.z;
 
-      $('#speed p').text('加速度X: ' + x);
-
-
-
-      if(Math.abs(x) > 10 || Math.abs(y) > 10 || Math.abs(z) > 10){
-        $('#speed p').text('振ったよ');
-      }
+    $('#speed p').text('加速度X: ' + x);
 
 
 
+    if(Math.abs(x) > 10 || Math.abs(y) > 10 || Math.abs(z) > 10){
+      $('#speed p').text('振ったよ');
+    }
+  }
 
 
 
+
+
+
+//----------------------------------------------------------------
+//キャラ選択画面
+  $('#charaSet li').hide();
+  $('#charaSet li:first').appendTo('#charaSet').fadeIn();
+
+  $('#charaName li').hide();
+  $('#charaName li:first').appendTo('#charaName').fadeIn();
 
   //----------------------------------------------------------------
-  //キャラ選択画面
-    $('#charaSet li').hide();
-    $('#charaSet li:first').appendTo('#charaSet').fadeIn();
-
-    $('#charaName li').hide();
-    $('#charaName li:first').appendTo('#charaName').fadeIn();
-
-    //----------------------------------------------------------------
-    //バトル画面ーキャラ表示
+  //バトル画面ーキャラ表示
 
 
-    $('#chara li').hide();
-    $('#chara li:first').appendTo('#chara').fadeIn();
+  $('#chara li').hide();
+  $('#chara li:first').appendTo('#chara').fadeIn();
 
-    $('#name li').hide();
-    $('#name li:first').appendTo('#name').fadeIn();
+  $('#name li').hide();
+  $('#name li:first').appendTo('#name').fadeIn();
 
-    //----------------------------------------------------------------
-    //キャラ選択ー移動
-
-
-    $('#right').on('click',function(){
-       console.log(count);
-       count++;
-       if(count > 5){
-         count = 0;
-       }
-
-       $('#charaSet li').eq(0).hide().appendTo('#charaSet').fadeIn();
-       $('#charaName li').eq(0).hide().appendTo('#charaName').fadeIn();
-       $('#chara li').eq(0).hide().appendTo('#chara').fadeIn();
-       $('#name li').eq(0).hide().appendTo('#name').fadeIn();
-     });
+  //----------------------------------------------------------------
+  //キャラ選択ー移動
 
 
-     $('#left').on('click',function(){
-        console.log(count);
-        count--;
-        if(count < 0){
-          count = 5;
-        }
-        $('#charaSet li').eq(4).show();
-        $('#charaSet li').eq(5).fadeOut(function(){
-          $('#charaSet li').eq(5).prependTo('#charaSet');
-          $('#charaSet li').eq(0).show();
-        });
+  $('#right').on('click',function(){
+     console.log(count);
+     count++;
+     if(count > 5){
+       count = 0;
+     }
 
-        $('#charaName li').eq(4).show();
-        $('#charaName li').eq(5).fadeOut(function(){
-          $('#charaName li').eq(5).prependTo('#charaName');
-          $('#charaName li').eq(0).show();
-        });
-
-        $('#chara li').eq(4).show();
-        $('#chara li').eq(5).fadeOut(function(){
-          $('#chara li').eq(5).prependTo('#chara');
-          $('#chara li').eq(0).show();
-        });
-
-        $('#name li').eq(4).show();
-        $('#name li').eq(5).fadeOut(function(){
-          $('#name li').eq(5).prependTo('#name');
-          $('#name li').eq(0).show();
-        });
+     $('#charaSet li').eq(0).hide().appendTo('#charaSet').fadeIn();
+     $('#charaName li').eq(0).hide().appendTo('#charaName').fadeIn();
+     $('#chara li').eq(0).hide().appendTo('#chara').fadeIn();
+     $('#name li').eq(0).hide().appendTo('#name').fadeIn();
+   });
 
 
-
-
+   $('#left').on('click',function(){
+      console.log(count);
+      count--;
+      if(count < 0){
+        count = 5;
+      }
+      $('#charaSet li').eq(4).show();
+      $('#charaSet li').eq(5).fadeOut(function(){
+        $('#charaSet li').eq(5).prependTo('#charaSet');
+        $('#charaSet li').eq(0).show();
       });
 
+      $('#charaName li').eq(4).show();
+      $('#charaName li').eq(5).fadeOut(function(){
+        $('#charaName li').eq(5).prependTo('#charaName');
+        $('#charaName li').eq(0).show();
+      });
+
+      $('#chara li').eq(4).show();
+      $('#chara li').eq(5).fadeOut(function(){
+        $('#chara li').eq(5).prependTo('#chara');
+        $('#chara li').eq(0).show();
+      });
+
+      $('#name li').eq(4).show();
+      $('#name li').eq(5).fadeOut(function(){
+        $('#name li').eq(5).prependTo('#name');
+        $('#name li').eq(0).show();
+      });
+
+
+
+
+    });
+
+//----------------------------------------------------------------
+//選ぶボタン
+
+
+  $('#select').on('click',function(){
+      $('#charaSelect').hide();
+      $('#battle').show();
+      $('#heart li').hide();
+      $('#dice li').hide();
+      $('#heart li').eq(life).appendTo('#heart').fadeIn();
+
+      console.log(count);
+
+  });
+
+
+
+
   //----------------------------------------------------------------
-  //選ぶボタン
-
-
-    $('#select').on('click',function(){
-        $('#charaSelect').hide();
-        $('#battle').show();
-        $('#heart li').hide();
-        $('#dice li').hide();
-        $('#heart li').eq(life).appendTo('#heart').fadeIn();
-
-        console.log(count);
-
-    });
+  //サイコロ画面
 
 
 
+  $('#rool').on('click',function(){
+    $('#rool').hide();
+    rand = Math.floor( Math.random() * 6 ) ;
+    console.log(rand);
 
-    //----------------------------------------------------------------
-    //サイコロ画面
-
-
-
-    $('#rool').on('click',function(){
-      $('#rool').hide();
-      rand = Math.floor( Math.random() * 6 ) ;
-      console.log(rand);
-
-      if(Math.abs(x) > 10 || Math.abs(y) > 10 || Math.abs(z) > 10){
-        $('#dice li').eq(rand).fadeIn(2000);
-      }
+    if(Math.abs(x) > 10 || Math.abs(y) > 10 || Math.abs(z) > 10){
+      $('#dice li').eq(rand).fadeIn(2000);
+    }
 
 
-      rand = 0;
+    rand = 0;
 
 
-    });
-}
+  });
+
 $('#chara').on('click',function(){
     $('#rool').show();
     $('#dice li').hide();
