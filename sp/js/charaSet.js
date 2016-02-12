e22f69390de7435d2d4b21698ef28d1284ba6e14
YukiKitagawa <Yuki-Kitagawa@kitagawayuuki-no-MacBook-Pro.local>
2016-02-12 16:37:03 +0900

    aa

--- sp/js/charaSet.js
+++ sp/js/charaSet.js
@@ -144,7 +144,7 @@ $(function(){
 
     if(Math.abs(x) > 10 || Math.abs(y) > 10 || Math.abs(z) > 10){
       // $('#speed p').text('振ったよ');
-      $('#dice li').eq(rand).show();
+      $('#dice li').eq(rand).fadeIn();
     }
   }
 
